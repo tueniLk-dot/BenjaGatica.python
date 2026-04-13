62bfd2b4fd8b8f54731237cd1871b2814f0a3b95
tueniLk <benjamin.gaticaurzua02@gmail.com>
2026-04-13 09:45:58 -0400

    ejemplo de varoables

--- prueba2.py
+++ prueba2.py
@@ -1,1 +1,20 @@
 print("Hola Luis")
+
+# creando variables
+
+titulo="clima del dia de hoy" #srting
+diaDelMes=13#int 
+mes=4 #int
+
+temperatura=22.3 #float
+
+llueve= False #boolean
+
+print(titulo)
+print("temperatura actual ", temperatura, "grados")
+print(diaDelMes," - ", mes)
+
+if llueve :
+    print("Tiene que llevar paragua")
+else:
+     print("puede llevar polera sin mangas")
