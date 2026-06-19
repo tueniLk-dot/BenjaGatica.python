787e739f56746568d828e4f000c6e83646d05536
tueniLk-dot <benjamin.gaticaurzua02@gmail.com>
2026-06-19 14:52:50 -0400

    clasepre prueba 4

---
+++ def/19.6.py
@@ -0,0 +1,90 @@
+# crear un gestor de peliculas
+# EL titulo debe tener mas de 2 caracteres
+# el año debe ser mayor a 1960 y debe der menor al año actual
+# El director debe tener nombre y apellido
+# mostar el sigueinte menú
+#  
+'''
+1.- ingresar Pelucula
+2.- quitar Pelucula
+3.- ingresar Pelucula
+4.- Mostar Peluculas
+5.- Mostrar solo los titulos
+6.- Ordenar de mas reciente a mas antigua
+7.- Salir
+'''
+
+pelis=[
+    {"tittle" : "Ice Age", "anio" : 2002, "director" : "Chris Wedge", "genero": "animado"},
+    {"tittle" : "Fast and forious", "anio" : 2001, "director" : "Rob Cohen", "genero": "accion"},
+    {"tittle" : "Marix", "anio" : 1999, "director" : "Lana Wachowski, Lilly Wachowski", "genero": "ciencia ficcion"},
+]
+def addPeli():
+    titulo=input("Ingresa un nuevo titulo: ")
+    anio=int(input("Ingresa año de la pelicula: "))
+    director=input("Ingrese al director de la pelicula: ")
+    genero=input("Ingresa el genero de la pelicula: ")
+    pelis.append({"tittle" : titulo, "anio" : anio, "director" : director, "genero": genero})
+    mostrarTitulos()
+def quitarPelis():
+    mostrarPeli()
+    peli=int(input("Que pelicula se quita ?: "))
+    pelis.pop(peli-1)
+    print("Pelicula eliminada ")
+
+
+def mostrarPeli():
+    if len(pelis)==0:
+        print("No hay peliculas")
+    else:
+        c=1
+        for p in pelis:
+            print(f"{c} .- {p}")
+            c+=1
+def mostrarTitulos():
+    for mostrar in pelis:
+        print(mostrar["tittle"])
+def salir():
+    print("Salida")
+
+while True:
+    try:
+        op=int(input('''
+        1.- ingresar Pelucula
+        2.- quitar Pelucula
+        3.- actualizar Pelicula
+        4.- Mostar Peluculas
+        5.- Mostrar solo los titulos
+        6.- Ordenar de mas reciente a mas antigua
+        7.- Salir
+        '''))
+        match op:
+            case 1:
+                addPeli()
+            case 2:
+                quitarPelis()
+            case 3:
+                mostrarPeli()    
+                actName=input("Que pelicula actualzara?: " )
+                pelis  [actName-1]=input("Cual sera el nuevo nombre?:  ")
+                print("actualizado: ", mostrarPeli()    )
+            case 4:
+                mostrarPeli()
+            case 5:
+                mostrarTitulos()
+            case 6:
+            
+                print
+            case 7:
+                salir()
+                break
+            case _:
+                print("Error de seleccion, reintente")
+    except Exception as e:
+        print("Seleccion invalida",e )
+
+
+
+
+
+
---
+++ def/pacientes.py
@@ -0,0 +1,83 @@
+## Crear un gestor de pacientes
+
+pacientes=[
+    {"nombre": " Aquiles Baeza", "prevision": "Fonasa", 
+     "temperatura":34.6, "grave": False}
+]
+
+'''crear al gestor de pacientes en un centro medico
+Para poner el nombre se debe validar que no este vacio 
+y ademas tenga mas de 8 caracteres
+Para la prevision de salud solo exiten 3 posibles valores
+Fonasa, Isapre, o Fodesa
+Al ingresar un paciente, se debe poner la temperatura
+Crear una funcion que valide si esta grave o no
+Para que este grave debe tener mas de 39°
+Cada atencion vale $25.000
+Los despcuentos corresponden a 
+FOnasa 54%
+Isapre 27%
+Fodesa 12,5%
+
+'''
+def ingresarPaciente():
+    nombre=input("Ingrese nombre: ")
+    prevision=input("Ingrese prevision: ")
+    temp=float(input("Ingrese temp: "))
+    pacientes.append({"nombre": nombre, "prevision": prevision, 
+                "temperatura":temp, "grave": validarEstado(temp)})
+    print("Paciente agregado al listado")
+def quitarPaciente():
+    mostrarPacientes()
+    paci=int(input("Que paciente se vá?: "))
+    pacientes.pop(paci-1)
+    print("Paciente eliminado.")
+def validarEstado(tempe):
+   if tempe>39:
+       return True 
+   else:
+       return False
+def mostrarPacientes():
+    if len(pacientes)==0:
+        print("No hay pacientes")
+    else:
+        c=1
+        for p in pacientes:
+            print(f"{c} .- {p}")
+            c+=1
+def cobrarAtencion():
+    mostrarPacientes()
+    total=0
+    paci=input("Ingresa el tipo de prevision ")
+    if paci=="fonasa":
+        25000*0.54
+    elif paci=="fodesa":
+        25000*0.73
+    elif paci=="isapre":
+        25000*0.875
+    print("el costo a pagar es: ",total )
+
+while True:
+    try:
+        print("1.- Ingresar paciente")
+        print("2.- Quitar paciente")
+        print("3.- Tomar Temperatura")
+        print("4.- Cobra atencion")
+        print("5.- Mostrar Pacientes")
+        print("9.- Salir")
+        op=int(input("Ingrese una opcion: "))
+        match op:
+            case 1:
+                ingresarPaciente()
+            case 2:
+                quitarPaciente()
+            case 3:
+                mostrarPacientes()
+                input("Aqee pacoemte leeremos la temperatura? ")
+                
+            case 4:
+                cobrarAtencion()
+            case 5:
+                print
+    except Exception as e:
+        print("Error. ",e)
